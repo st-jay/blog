95e3bdd1a53221e2c476abb653535c9f03e6a6ab
Jay Mistry <92283713+st-jay@users.noreply.github.com>
2022-03-02 11:54:59 +0530

    adds a robots noindex for the deploy previews

--- src/html.js
+++ src/html.js
@@ -5,6 +5,7 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
       <head>
+        <meta name="robots" content="noindex" />
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
